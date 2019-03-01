8fd767d8c4c9e482017ba090dc9ef648752ba7a8
dstroppolo <stroppolo.daniel@gmail.com>
2019-03-01 02:04:15 -0500

    Made changes to original testing class instead of creating new one

--- src/test/java/org/springframework/samples/petclinic/owner/PetTypeFormatterTests.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetTypeFormatterTests.java
@@ -10,10 +10,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 /**
  * Test class for {@link PetTypeFormatter}
@@ -21,36 +21,48 @@ import static org.junit.Assert.assertEquals;
  * @author Colin But
  */
 @RunWith(MockitoJUnitRunner.class)
-public class PetTypeFormatterTests {
+public class PetTypeFormatterTestsNew {
 
     @Mock
     private PetRepository pets;
+    
+    private List<PetType> petTypes;
 
     private PetTypeFormatter petTypeFormatter;
 
     @Before
     public void setup() {
         this.petTypeFormatter = new PetTypeFormatter(pets);
+        //cant mock inside of thenReturn() so do it before
+        this.petTypes = makePetTypes();
+        when(this.pets.findPetTypes()).thenReturn(this.petTypes);
     }
 
     @Test
     public void testPrint() {
-        PetType petType = new PetType();
-        petType.setName("Hamster");
+    	//We're not testing if the PetType class gets the proper Name, we're testing if the print method
+    	//calls the getName to retrieve it. Other tests should exist to test the getName method.
+        PetType petType = mock(PetType.class);
+        when(petType.getName()).thenReturn("hamster");
         String petTypeName = this.petTypeFormatter.print(petType, Locale.ENGLISH);
-        assertEquals("Hamster", petTypeName);
+        verify(petType).getName();
+        assertEquals(petTypeName, "hamster");
+    }
+    
+    @Test
+    public void shouldParseFirstItem() throws ParseException {
+    	PetType petType = petTypeFormatter.parse("Dog", Locale.ENGLISH);
+        assertEquals("Dog", petType.getName());
     }
 
     @Test
-    public void shouldParse() throws ParseException {
-        Mockito.when(this.pets.findPetTypes()).thenReturn(makePetTypes());
+    public void shouldParseSecondItem() throws ParseException {
         PetType petType = petTypeFormatter.parse("Bird", Locale.ENGLISH);
         assertEquals("Bird", petType.getName());
     }
 
     @Test(expected = ParseException.class)
     public void shouldThrowParseException() throws ParseException {
-        Mockito.when(this.pets.findPetTypes()).thenReturn(makePetTypes());
         petTypeFormatter.parse("Fish", Locale.ENGLISH);
     }
 
@@ -61,16 +73,14 @@ public class PetTypeFormatterTests {
      */
     private List<PetType> makePetTypes() {
         List<PetType> petTypes = new ArrayList<>();
-        petTypes.add(new PetType() {
-            {
-                setName("Dog");
-            }
-        });
-        petTypes.add(new PetType() {
-            {
-                setName("Bird");
-            }
-        });
+        PetType dog = mock(PetType.class);
+        PetType bird = mock(PetType.class);
+        when(dog.getName()).thenReturn("Dog");
+        when(bird.getName()).thenReturn("Bird");
+        
+        petTypes.add(dog);
+        petTypes.add(bird);
+        
         return petTypes;
     }
 
