305d4c8e473ddc3a031ce1ec8c63142e47a53b75
Youssef <youssef.akallal@gmail.com>
2019-03-20 20:58:35 -0400

    set up sqlite database

--- src/main/java/org/springframework/samples/petclinic/config/SQLiteDialect.java
+++ src/main/java/org/springframework/samples/petclinic/config/SQLiteDialect.java
@@ -1,3 +1,5 @@
+package org.springframework.samples.petclinic.config;
+
 import org.hibernate.dialect.Dialect;
 
 import java.sql.Types;
@@ -9,6 +11,23 @@ public class SQLiteDialect extends Dialect {
         registerColumnType(Types.TINYINT, "tinyint");
         registerColumnType(Types.SMALLINT, "smallint");
         registerColumnType(Types.INTEGER, "integer");
-        // other data types
+        registerColumnType(Types.BIGINT, "bigint");
+        registerColumnType(Types.FLOAT, "float");
+        registerColumnType(Types.REAL, "real");
+        registerColumnType(Types.DOUBLE, "double");
+        registerColumnType(Types.NUMERIC, "numeric");
+        registerColumnType(Types.DECIMAL, "decimal");
+        registerColumnType(Types.CHAR, "char");
+        registerColumnType(Types.VARCHAR, "varchar");
+        registerColumnType(Types.LONGVARCHAR, "longvarchar");
+        registerColumnType(Types.DATE, "date");
+        registerColumnType(Types.TIME, "time");
+        registerColumnType(Types.TIMESTAMP, "timestamp");
+        registerColumnType(Types.BINARY, "blob");
+        registerColumnType(Types.VARBINARY, "blob");
+        registerColumnType(Types.LONGVARBINARY, "blob");
+        registerColumnType(Types.BLOB, "blob");
+        registerColumnType(Types.CLOB, "clob");
+        registerColumnType(Types.BOOLEAN, "integer");
     }
 }
